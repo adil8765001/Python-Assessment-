e713cca29132bda59ff09d1937b2cdf382fb012a
adil8765001 <adil8765001@gmail.com>
2026-06-24 19:10:27 -0400

    Add files via upload

---
+++ For_Loop_Exercise.py
@@ -0,0 +1,307 @@
+"""
+PART 1 – Basic For Loop Questions
+
+Q1. Print Numbers
+Use a for loop to print numbers from 1 to 10.
+
+for i in range(1,11):
+    print(i)
+
+num = 0
+for i in range(1,11):
+    num +=1
+    print(num)
+
+Q2. Print Even Numbers
+Print all even numbers between 1 and 20
+
+for i in range(2,21,2):
+    print(i)
+
+num = 0
+for i in range(1,11):
+    num +=2
+    print(num)
+
+Q3. Find Sum
+Print the sum of numbers from 1 to 10 using a for loop.
+
+su = 0
+for i in range(1,11):
+    su +=i
+    print(i)
+print("sum of all number ",su)
+
+
+Q4. Multiplication Table
+Take a number from the user and print its multiplication table up to 10.
+
+table = int(input("Enter a number to print multiplication table : "))
+
+for i in range(1,11):
+    print(f"{table} * {i} = {table*i}")
+
+
+Q5. Count Characters
+Take a string and count the total number of characters using a for loop.
+
+st = input("Enter a String : ")
+count = 0
+for char in st:
+    count += 1
+print("Total number of characters:", count)
+
+
+
+PART 2 – Break Related Questions
+
+
+
+Q6. Stop at 5
+Print numbers from 1 to 10.
+Stop the loop when the number becomes 5.
+
+for i in range(1,11):
+    print(i)
+    if i == 5:
+        break
+
+Q7. Search in List
+Search for number 25 in a list.
+If found, print "Found" and stop the loop.
+
+li = [12,24,23,65,68,98,97,69,25,23,54,76,27,84,37,59,35]
+for num in li:
+    print(num)
+    if num == 25:
+        print("Found")
+        break
+
+
+Q8. First Negative Number
+Given a list of numbers, print the first negative number and stop the loop.
+
+li = [12,24,23,65,68,-98,97,-69,25,-23,-54,-76,-27,84,37,59,35]
+for num in li:
+    print(num)
+    neg = num
+    if neg < 0:
+        print(neg)
+        break
+
+PART 3 – Continue Related Questions
+
+Q9. Skip 5
+Print numbers from 1 to 10.
+Skip number 5.
+
+for i in range(1,11):
+    if i == 5:
+        continue
+    print(i)
+
+
+Q10. Skip Even Numbers
+Print numbers from 1 to 20.
+Skip all even numbers.
+
+for i in range(1,21):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+
+Q11. Skip Letter
+Print each character of the string "PYTHON".
+Skip the letter "O".
+
+
+st = "PYTHON"
+
+for char in st:
+    if char == 'O':
+        continue
+    print(char)
+
+
+
+PART 4 – Pass Related Questions
+Q12. Empty Loop
+Run a loop from 1 to 5 but do nothing inside the loop using pass.
+
+for i in range(1,6):
+    pass
+
+
+Q13. Skip Using Pass
+Loop from 1 to 10.
+If number is 6, just use pass.
+
+
+for i in range(1,11):
+    if i == 6:
+        pass
+    print(i)
+
+
+
+
+PART 5 – For-Else Questions
+(Remember: else runs only if the loop is not stopped by break.)
+
+Q14. Search Number Using for-else
+Search for number 100 in a list.
+If found, print "Found".
+If not found, print "Not Found".
+
+
+num = [20,30,50,60,100,120,40]
+
+for i in num:
+    print(i)
+    if i == 100:
+        print("Found")
+        break
+else:
+    print("Not Found")
+
+
+
+Q15. Prime Number Check
+Take a number from the user and check whether it is prime using for-else.
+
+
+num = int(input("Enter a number :- "))
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+
+
+PART 6 – Pattern Questions
+
+
+Q16. Star Pattern
+Print:
+*
+**
+***
+****
+*****
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end = '')
+    print()
+
+Q17. Reverse Star Pattern
+Print:
+*****
+****
+***
+**
+*
+
+rows = 6
+for i in range(6,0,-1):
+    for j in range(1,i):
+        print("*",end = '')
+    print()
+
+Q18. Number Pattern
+Print:
+1
+12
+123
+1234
+12345
+
+num = ""
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end='')
+    print()
+
+Q19. Same Number Pattern
+Print:
+1
+22
+333
+4444
+55555
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i,end='')
+    print()
+
+
+Q20. Pyramid Pattern
+Print:
+    *
+   ***
+  *****
+ *******
+*********
+
+for i in range(1,6):
+    for k in range(5,i,-1):
+        print(" ",end='')
+    for j in range(1,i*2):
+        print('*',end='')
+    print()
+
+Q21. Inverted Pyramid
+
+Print:
+***********
+ *********
+  *******
+   *****
+    ***
+     *
+
+rows = 6
+for i in range(rows,0,-1):
+    for k in range(6,i,-1):
+        print(" ",end='')
+    for j in range(1,i*2):
+        print('*',end='')
+    print()
+
+
+Bonus Question
+Q22. Break in Pattern
+Print a star pattern.
+Stop printing when the row number reaches 4.
+
+for i in range(1,6):
+    if i>4:
+        break
+    for j in range(1,i+1):
+        print("*",end="")
+    print()
+
+"""
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
